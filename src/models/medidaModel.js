942341949ab7e4033f5873592fe4a5917ce0b3d0
ArthurMatos1 <arthur.matos@sptech.school>
2025-12-02 20:06:05 -0300

    Subindo os selects para atualização das kpis

--- src/models/medidaModel.js
+++ src/models/medidaModel.js
@@ -29,7 +29,22 @@ function alertasDash(fkEmpresaUser) {
 
 function mostrarKpis(fkEmpresaUser) {
 
-    var instrucaoSql = ``;
+    var instrucaoSql = `SELECT 
+    (SELECT m.umidade FROM medicao AS m ORDER BY m.idMedicao DESC LIMIT 1) as umiatual,
+    (SELECT MAX(m.umidade) FROM medicao AS m) AS maxumi,
+    (SELECT DATE_FORMAT(m.dtMedicao, '%Hh%i') FROM medicao m
+	WHERE m.umidade = (SELECT MAX(m.umidade) FROM medicao m)) AS hrMax,
+    (SELECT MIN(m.umidade) FROM medicao AS m) as minumi,
+	(SELECT COUNT(*) FROM vwAlertas)as qtdOcorrencia,
+	(SELECT DATE_FORMAT(m.dtMedicao, '%Hh%i') FROM medicao m
+	WHERE m.umidade = (SELECT MIN(m.umidade) FROM medicao m)) AS hrMin
+    FROM medicao AS m JOIN sensor s ON m.fksensor = s.idSensor
+		JOIN subarea sa ON s.fkSub = sa.idSubArea
+			JOIN hectare h ON sa.fkHectare = h.idHectare
+				JOIN empresa e ON h.fkEmpresaHect = e.codAtivacao
+                WHERE e.codAtivacao = '${fkEmpresaUser}'
+                GROUP BY m.umidade
+					ORDER BY m.umidade LIMIT 1;`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
