56e572812db1fbe724a8af7940c515c39544231c
Cesar409-dot <cesar.vequiato@sptech.school>
2025-12-05 19:15:59 -0300

    Subindo bobIA implementada para o usuário suporte n3 utilizar

--- app.js
+++ app.js
@@ -7,11 +7,15 @@ var caminho_env = ambiente_processo === 'producao' ? '.env' : '.env.dev';
 
 require("dotenv").config({ path: caminho_env });
 
+var { GoogleGenAI } = require("@google/genai");
 var express = require("express");
 var cors = require("cors");
 var path = require("path");
 var PORTA_APP = process.env.APP_PORT;
 var HOST_APP = process.env.APP_HOST;
+var PORTA_SERVIDOR = process.env.PORTA;
+const chatIA = new GoogleGenAI({ apiKey: process.env.MINHA_CHAVE });
+
 
 var app = express();
 
@@ -23,6 +27,12 @@ var medidasRouter = require("./src/routes/medidas");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
+    next();
+});
 
 app.use(cors());
 
@@ -47,3 +57,51 @@ app.listen(PORTA_APP, function () {
     \tSe .:producao:. você está se conectando ao banco remoto. \n\n
     \t\tPara alterar o ambiente, comente ou descomente as linhas 1 ou 2 no arquivo 'app.js'\n\n`);
 });
+
+app.listen(PORTA_SERVIDOR, () => {
+    console.info(
+        `
+        ######                ###    #    
+        #     #  ####  #####   #    # #   
+        #     # #    # #    #  #   #   #  
+        ######  #    # #####   #  #     # 
+        #     # #    # #    #  #  ####### 
+        #     # #    # #    #  #  #     # 
+        ######   ####  #####  ### #     # 
+        `
+    );
+    console.info(`A API BobIA iniciada, acesse http://${HOST_APP}:${PORTA_APP}/bob.html`);
+});
+
+app.post("/perguntar", async (req, res) => {
+    const pergunta = req.body.pergunta;
+
+    try {
+        const resultado = await gerarResposta(pergunta);
+        res.json({ resultado });
+    } catch (error) {
+        res.status(500).json({ error: 'Erro interno do servidor' });
+    }
+
+});
+
+async function gerarResposta(mensagem) {
+
+    try {
+        const modeloIA = chatIA.models.generateContent({
+            model: "gemini-2.0-flash",
+            contents: `Em um paragráfo responda: ${mensagem}`
+
+        });
+        const resposta = (await modeloIA).text;
+        const tokens = (await modeloIA).usageMetadata;
+
+        console.log(resposta);
+        console.log("Uso de Tokens:", tokens);
+
+        return resposta;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
